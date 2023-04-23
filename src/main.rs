f91b08aab4d3fe6e6603f5e5347afc50a0072f12
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2023-04-24 00:45:32 +0200

    Enter all tile data, display it nicely

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,32 @@ extern crate lazy_static;
 mod tile;
 
 fn main() {
-    println!("Hello, world! -> {:?}", *tile::ALL_TILES);
+    let mut is_first_tile = true;
+    for tile in tile::ALL_TILES.iter() {
+        if !is_first_tile {
+            println!("==");
+        } else {
+            is_first_tile = false;
+        }
+        let mut is_first_layout = true;
+        for layout in tile.get_layouts() {
+            if !is_first_layout {
+                println!("-");
+            } else {
+                is_first_layout = false;
+            }
+            for y in 0..tile::MAX_SIZE {
+                print!("    ");
+                for x in 0..tile::MAX_SIZE {
+                    let symbol = if layout.is_present_at(x, y) {
+                        'X'
+                    } else {
+                        '·'
+                    };
+                    print!("{}", symbol);
+                }
+                println!();
+            }
+        }
+    }
 }
